334725762652880b95bfcff83ac654c3d453b6b4
Smaeel12 <ismailboubkri2002@gmail.com>
2024-02-20 16:26:49 +0000

    1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,4 +7,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *prev;
+
+	current = (*list)->next;
+	while (current)
+	{
+		prev = current->prev;
+		while (prev && prev->n > current->n)
+		{
+			if (prev->prev)
+				prev->prev->next = current;
+			if (current->next)
+				current->next->prev = prev;
+			prev->next = current->next;
+			current->prev = prev->prev;
+			prev->prev = current;
+			current->next = prev;
+
+			prev = current->prev;
+			if (prev == NULL)
+				*list = current;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
